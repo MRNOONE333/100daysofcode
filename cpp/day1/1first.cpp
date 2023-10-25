5ec83595f9b12f576ccc0566aa47344a59b16bdc
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2023-10-25 20:08:08 +0530

    day14 && day 1 of cpp

---
+++ cpp/day1/1first.cpp
@@ -0,0 +1,36 @@
+//  printing output.
+
+// 1
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     cout << "flgzjkb";
+//         cout << "11flgzjkb";
+//     return 0 ;
+// }
+
+/*
+output- 
+flgzjkb11flgzjkbṇ
+*/
+
+
+// 2.
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "flgzjkb" << endl;
+        cout << "11flgzjkb";
+    return 0 ;
+}
+
+/*
+output- 
+flgzjkb
+11flgzjkb
+*/
+
+
+
