7ba105a2f0398ca942403ecde8bd9ca67d0b6f15
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2023-11-01 00:00:03 +0530

    day 20

---
+++ dsa_practice_start/2dynamicMemoryAllocation/4dynnamicaAllocation.cpp
@@ -0,0 +1,19 @@
+// see cell7 of notes.
+#include <iostream>
+using namespace  std;
+
+
+int main(){
+    int *p = new int;
+    cout<<*p;
+    delete p;
+
+    int *p = new int [5];
+    cout<<*p;
+    delete []p;
+    cout<<*p;
+
+    // address isn't deleted in any
+
+
+}
