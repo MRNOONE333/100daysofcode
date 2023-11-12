bd558e6853faea017b795f9db043557ce93c1c36
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2023-11-12 23:38:16 +0530

    day31(ii)

--- dsa_practice_start/3.recursion/Assignment-2/6PrintSubsetSumToK.cpp
+++ dsa_practice_start/3.recursion/Assignment-2/6PrintSubsetSumToK.cpp
@@ -18,3 +18,57 @@
 // Sample Output:
 // 3 3
 // 5 1
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printSubsetsSumKHelper(int arr[], int n, int k, vector<int>& subset, int sum, int index) {
+    // If the current subset has the desired sum, print it
+    if (sum == k) {
+        for (int i : subset) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+
+    // Generate all subsets that include the current element
+    for (int i = index; i < n; i++) {
+        // Include the current element in the subset
+        subset.push_back(arr[i]);
+
+        // Recursively generate subsets starting from the next index
+        printSubsetsSumKHelper(arr, n, k, subset, sum + arr[i], i + 1);
+
+        // Exclude the current element from the subset (backtrack)
+        subset.pop_back();
+    }
+}
+
+void printSubsetsSumK(int arr[], int n, int k) {
+    vector<int> subset;
+    printSubsetsSumKHelper(arr, n, k, subset, 0, 0);
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the array elements separated by space: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int k;
+    cout << "Enter the value of K: ";
+    cin >> k;
+
+    cout << "Subsets with sum equal to " << k << " are:" << endl;
+    printSubsetsSumK(arr, n, k);
+
+    return 0;
+}
